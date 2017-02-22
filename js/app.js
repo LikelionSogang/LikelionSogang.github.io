20e1acf8dc048710b64d6bb85290d308e362f1f5
JISEUNG ROH <jiseung.roh@gmail.com>
2017-02-22 17:35:06 +0900

    remove highlighting animation when accessed via touchscreen devices

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  var highlight_list = Array.prototype.slice.call(document.querySelectorAll('.highlight-green span'));
+  console.log(highlight_list);
+  highlight_list.forEach(function(item){
+    item.setAttribute('class', 'mobile');
+  });
+}
+
 var sections = Array.from(document.querySelectorAll('body > section')).slice(1,4);
 
 
